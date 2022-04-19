5c87cd428dffb6e95b4b6c80eb99482aa9cbabc5
Shawn Hassen <shawn@solvvy.com>
2022-04-19 11:22:08 -0400

    finished temporal deadzone work

--- practice.js
+++ practice.js
@@ -1,17 +1,60 @@
-console.log("hello world!");
-// name var
-const firsName = "shawn";
+// The Complete Javascript Course 2022: Section 8: How JS works behind the scenes
 
-// example to show hoisting, if job is defined after the console.log, it will get an initialization error
-if (firsName === "shawn") {
-  const job = "engineer";
-  const age = 2022 - 1989;
-  console.log("age: ", age);
-  console.log(`Shawn is a ${job}`);
+// ----- Temporal Deadzone Practice (TDZ) ------
+
+// main notes (programming best practice):
+// - Always define vars at the top of code
+// - Always use const or let, never var
+// - Always declare all functions first and use only after declaration
+// - let and const are not declared on the window object but var is
+
+// this example shows that all variable types need to be defined before used in the code
+// var's will be undefined if called before initialization, let and const will be a ref error not initialized yet
+
+var me = "Shawn";
+let job = "Engineer";
+const year = 1989;
+
+console.log("me: ", me);
+console.log("job: ", job);
+console.log("year: ", year);
+
+// Functions
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-// Temporal Deadzone Practice (TDZ)
+const addExpr = function (a, b) {
+  return a + b;
+};
 
-console.log(me);
-console.log();
-console.log();
+var addArrow = (a, b) => {
+  return a + b;
+};
+
+console.log("add decl: ", addDecl(2, 3));
+console.log("add expr: ", addExpr(4, 5));
+console.log("add arrow: ", addArrow(6, 7));
+
+// pitfalls of hoisting:
+// in the block below, if the var is above if block, it will work. If var is below if, this will execute prematurely
+
+// if (!numProducts) {
+//   deleteShoppingCart();
+// }
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
+}
+
+// this shows that vars are declared on the window object and let, const are not
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log("x: ", x === window.x); // true
+console.log("y: ", y === window.y); // false
+console.log("z: ", z === window.z); // false
