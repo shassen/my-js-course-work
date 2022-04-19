4a1476c91fa27511f2c70bc974261b24f85a0829
Shawn Hassen <shawn@solvvy.com>
2022-04-19 11:02:52 -0400

    setting up file for temporal deadzone practice (TDZ)

--- practice.js
+++ practice.js
@@ -1,1 +1,17 @@
 console.log("hello world!");
+// name var
+const firsName = "shawn";
+
+// example to show hoisting, if job is defined after the console.log, it will get an initialization error
+if (firsName === "shawn") {
+  const job = "engineer";
+  const age = 2022 - 1989;
+  console.log("age: ", age);
+  console.log(`Shawn is a ${job}`);
+}
+
+// Temporal Deadzone Practice (TDZ)
+
+console.log(me);
+console.log();
+console.log();
